f6b9dc124ea0f312326d17a52e9b83d52faa60f2
Damyan Peykov <dppeykov@gmail.com>
2019-08-28 16:20:42 +0200

    process.exit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => console.log("Helloooo"));
+// managed by the event loop -
+// keeps running as long as there are event listeners registered
+const server = http.createServer((req, res) => {
+  console.log("Helloooo");
+  //process.exit(); //if we want to end the node process
+});
 
 server.listen(8080);
