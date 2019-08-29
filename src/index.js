4d1b687d0b3cb66f602a24340d89ad7aef64a936
Damyan Peykov <dppeykov@gmail.com>
2019-08-29 08:58:18 +0200

    ReqRes basics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const http = require("http");
 
 // managed by the event loop -
 // keeps running as long as there are event listeners registered
+
+//REQUESTS - client -> server
 const server = http.createServer((req, res) => {
   console.log(
     `
@@ -12,6 +14,14 @@ const server = http.createServer((req, res) => {
     req.headers
   );
   //process.exit(); //if we want to end the node process
+
+  //RESPONSES - server -> client
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my NodeJS server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(8080);
