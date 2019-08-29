e4df70f76226194bd17aac2b84d1342f63059eb1
Damyan Peykov <dppeykov@gmail.com>
2019-08-29 08:50:11 +0200

    Requests basics

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ const http = require("http");
 // managed by the event loop -
 // keeps running as long as there are event listeners registered
 const server = http.createServer((req, res) => {
-  console.log("Helloooo");
+  console.log(
+    `
+    URL: ${req.url}, 
+    METHOD: ${req.method}, 
+    HEADERS: 
+    `,
+    req.headers
+  );
   //process.exit(); //if we want to end the node process
 });
 
